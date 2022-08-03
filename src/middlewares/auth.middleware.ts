bcf70586e2ff67ce63cc7d00fcbdec5192a566f6
Emmanuel SARPEDON <emmanuel.sarpedon@gmail.com>
2022-08-03 15:51:13 +0400

    fix: Auth middleware - return badCredentials error if user doesn't exist on database even if token is good

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,13 +2,14 @@ import { NextFunction, Request, Response } from "express";
 import jwt, { Secret } from "jsonwebtoken";
 import * as error from "~/errors/errors";
 import { User } from "~/models/user.model";
+import { getUserById } from "~/services/user.services";
 
 export interface CustomRequest extends Request {
    user?: User;
    isAuthenticated?: boolean;
 }
 
-export function requiredAuth(
+export async function requiredAuth(
    req: CustomRequest,
    res: Response,
    next: NextFunction
@@ -18,12 +19,14 @@ export function requiredAuth(
    if (!token) return error.badCredentials(res);
 
    try {
-      req.user = jwt.verify(
-         token,
-         process.env.JWT_SECRET_KEY as Secret
-      ) as User;
-      req.isAuthenticated = true;
+      //prettier-ignore
+      const userDecodeFromJWT = jwt.verify( token, process.env.JWT_SECRET_KEY as Secret ) as User;
+      const userFromDatabase = await getUserById((userDecodeFromJWT.id).toString());
 
+      if (userFromDatabase === null) return error.badCredentials(res);
+
+      req.user = userDecodeFromJWT;
+      req.isAuthenticated = true;
       next();
    } catch (e) {
       return error.badCredentials(res);
